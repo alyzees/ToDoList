e2e52c21b02b57b6ccbebe8cd15b712631c3a068
Alyzee Sosa <alyzeesosacollege@gmail.com>
2023-07-18 10:58:30 -0700

    creating files and title

--- ToDoList/ContentView.swift
+++ ToDoList/ContentView.swift
@@ -9,13 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        ScrollView{
+            VStack {
+                HStack{
+                    Text("To Do List").font(.system(size: 40, weight: .bold)).padding(15.0)
+                    
+                    // another way to create a button is to put the action as a parameter, and the inside of the {} is just the text content
+                    Spacer(minLength: 35.0)
+                    Button(action: {
+                        // action
+                    }){
+                        Text("+").font(.system(size: 35, weight: .bold))
+                    }.padding([.top, .bottom, .trailing], 20.0)
+                    
+                }.frame(maxWidth: .infinity) // might be unnecessary b/c of spacer ==> rather the contents should be max width
+            }
+            
+        } // end of scrollview
     }
 }
 
---
+++ ToDoList/NewToDoView.swift
@@ -0,0 +1,20 @@
+//
+//  NewToDoView.swift
+//  ToDoList
+//
+//  Created by Scholar on 7/18/23.
+//
+
+import SwiftUI
+
+struct NewToDoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NewToDoView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewToDoView()
+    }
+}
